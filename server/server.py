2aa0ef9ea3333563095031e81466dfd4087a5a59
Will Russell <will@will-russell.com>
2020-01-09 21:11:47 +0000

    Added time limit

--- server/server.py
+++ server/server.py
@@ -1,4 +1,17 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import datetime
+
+time_limit = 1 # 1 minute is the default time limit
 
 def main():
-    pass
+    app = Flask(__name__)
+
+    @app.route('/', methods=['GET'])
+    def get_time_limit():
+
+        now = datetime.datetime.now()
+        end = now + datetime.timedelta(0,0,0,0, time_limit)
+
+        return jsonify({'time' : end})
+
+    app.run(port=3001)
