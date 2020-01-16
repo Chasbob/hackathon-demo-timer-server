12b5a616ae70fdb1e603bc46bbc35437cbe048de
Will Russell <will@will-russell.com>
2020-01-16 01:53:03 +0000

    Added set_time_limit function

--- server/server.py
+++ server/server.py
@@ -1,13 +1,13 @@
 from flask import Flask, request, jsonify
 import datetime
 
-time_limit = 1 # 1 minute is the default time limit
+time_limit = 3 # 1 minute is the default time limit
 
 def main():
     app = Flask(__name__)
 
-    @app.route('/', methods=['GET'])
-    def get_time_limit():
+    @app.route('/get_countdown', methods=['GET'])
+    def countdown():
 
         now = datetime.datetime.now()
         end = now + datetime.timedelta(0,0,0,0, time_limit)
@@ -15,3 +15,11 @@ def main():
         return jsonify({'time' : end})
 
     app.run(port=3001)
+
+    @app.route('/set_time_limit', methods=['GET', 'POST'])
+    def set_time(time):
+        try:
+            if request.method = "POST":
+                time_limit = request.data['time_limit']
+        except Exception as e:
+            print(e)
