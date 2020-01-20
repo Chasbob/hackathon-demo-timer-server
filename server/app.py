8ddd629d0ac85997b1971e17ae864f5739fcc026
Will Russell <will@will-russell.com>
2020-01-20 12:12:44 +0000

    Renamed to app

--- server/app.py
+++ server/app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 import datetime
 
 time_limit = 3 # 1 minute is the default time limit
+end_time = 0
 
 def main():
     app = Flask(__name__)
@@ -10,16 +11,16 @@ def main():
     def countdown():
 
         now = datetime.datetime.now()
-        end = now + datetime.timedelta(0,0,0,0, time_limit)
+        endtime = now + datetime.timedelta(0,0,0,0, time_limit)
 
-        return jsonify({'time' : end})
+        return jsonify({'time' : endtime})
 
     app.run(port=3001)
 
     @app.route('/set_time_limit', methods=['GET', 'POST'])
     def set_time(time):
         try:
-            if request.method = "POST":
+            if request.method == "POST":
                 time_limit = request.data['time_limit']
         except Exception as e:
             print(e)
